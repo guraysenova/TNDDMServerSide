b69c0bfaa4f35ffc4c266e6a86f71345acc9e2cd
M. Guray Senova <guraysenova@outlook.com>
2020-08-16 16:51:00 +0300

    Create TokenGenerator.cs

---
+++ TNDDMMatchServer/Classes/Token/TokenGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace TNDDMMatchServer
+{
+    public static class TokenGenerator
+    {
+        public static string GetNewToken()
+        {
+            byte[] bytes = new byte[40];
+
+            using (var crypto = new RNGCryptoServiceProvider())
+            {
+                crypto.GetBytes(bytes);
+            }
+
+            return BitConverter.ToString(bytes).Replace("-", "").ToLower();
+        }
+    }
+}
